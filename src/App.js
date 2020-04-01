1e1e644298a79cfd0f9005d0ea453b4302a4487f
Chetan Verma <chetan61verma@gmail.com>
2020-04-01 14:31:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import About from './components/About';
+import Landing from './components/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/about">
+        <About></About>
+        </Route>
+        <Route path="/" exact>
+        <Landing></Landing>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/yourdata.js
@@ -0,0 +1,95 @@
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+
+  //Change The Website Template
+  name: "chetanverma.",
+
+  //Contact Email
+  contactEmail: "hello@chetanverma.com",
+
+  // Landing Page Name
+  landingPageName: "Chetan Verma",
+
+  // Landing Page Paragraph
+  landingPagePara:
+    "Designer and front-end developer. I work as the Creative Director at WORK in India, while also taking on freelance projects that pique my interest.",
+
+  // Landing Page Image (replace - root/public/assets/photo.jpg)
+  landingPageImage: "assets/photo.jpg",
+
+  // Change Projects Here
+  projects: [
+    {
+      id: 1, //DO NOT CHANGE THIS (Please)😅
+      title: "Project One", //Project Title - Add Your Project Title Here
+      // Image Size Should be (300X300 px)
+      imageSrc: "https://via.placeholder.com/300",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/"
+    },
+    {
+      id: 2, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Two",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "http://chetanverma.com/"
+    },
+    {
+      id: 3, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Three",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "http://chetanverma.com/"
+    },
+    {
+      id: 4, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Four",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "http://chetanverma.com/"
+    },
+    {
+      id: 5, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Five",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "http://chetanverma.com/"
+    },
+    {
+      id: 6, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Six",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "http://chetanverma.com/"
+    }
+    // Paste more projects here from the template
+
+    /*
+                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+                ,{
+                id: 7,
+                service: 'Something Amazing',
+                imageSrc: "",
+                url: ''
+            }
+                */
+  ],
+
+  // Contact Section
+  contactPara:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  social: [
+    // Add Or Remove The Link Accordingly
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/chetanverma7/"
+    },
+    {
+      name: "Github",
+      url: "https://www.behance.net/chetanverma"
+    },
+    {
+      name: "Dribbble",
+      url: "https://dribbble.com/chetanverma"
+    }
+  ],
+
+  // About Page
+  aboutTitle: "I make remarkable digital products",
+  aboutPara:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy text eversince the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+};
